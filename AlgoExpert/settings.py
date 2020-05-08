6d3aa75a1fc220d0788240d9041d09d2b2025539
Tran Minh Hai <haitm@chainos.vn>
2020-05-08 20:39:16 +0700

    20% creating feedback and algorithms models

--- AlgoExpert/settings.py
+++ AlgoExpert/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_(3=72ke#i%e%3vf#kq()&m=qtool!pxo@#!$-9pqi)t#3c*sk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'algorithms',
+    'feedbacks'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'AlgoExpert.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- AlgoExpert/urls.py
+++ AlgoExpert/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from algorithms.views import home_view, algorithm_detail_view, algorithm_list_view
+from feedbacks.views import feedback_detail_view, feedback_list_view, feedback_create_view, profile_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
+    path('home', home_view, name="Home"),
+    path('profile', profile_view, name="Profile"),
+    path('', home_view, name="Home"),
+    path('algorithms', algorithm_list_view),
+    path('algorithms/<int:algorithm_id>', algorithm_detail_view),
+    path('feedbacks', feedback_list_view),
+    path('feedbacks/<int:feedback_id>', feedback_detail_view),
+    path('feedbacks/create', feedback_create_view),
 ]
